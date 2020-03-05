06c26920f2494e0ad33af7d3d58f8dc1476c5e45
Andrii Veldymanov <Andrii_Veldymanov@epam.com>
2020-03-05 15:58:49 +0200

    add app-shell

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ ng add @angular/material
 ng g m shared
 ng g m core
 
-ng add @angular/pwa
-ng g app-shell
-
 setup small 'server'
 create 'proxy.json' file
 copy the App
+
+ng add @angular/pwa
+ng g app-shell
 
 
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LessonsComponent } from "./lessons/lessons.component";
+// import { AppShellComponent } from './app-shell/app-shell.component';
+
 
 const routes: Routes = [
   {
     path: '',
     component: LessonsComponent
-  }
+  },
+  // { path: '', component: AppShellComponent }
 ];
 
 @NgModule({
